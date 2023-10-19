a9a0e6a467b2987f873bc565d8584d4ca72f469e
Черняков Григорий Валерьевич <chgv@minsk.bgtg.by>
2023-10-19 14:10:40 +0300

    fix problem with layers

--- data/granici_SK63_C_zone_5.js
+++ data/granici_SK63_C_zone_5.js
@@ -1,8 +1,1 @@
-var json_granici_SK63_C_zone_5 = {
-"type": "FeatureCollection",
-"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
-"features": [
-{ "type": "Feature", "properties": { "qc_id": 0, "name": "63-1/2", "descriptio": null, "timestamp": null, "begin": null, "end": null, "altitudemo": null, "tessellate": 1, "extrude": 0, "visibility": -1, "draworder": null, "icon": null }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ 26.450084489033092, 51.816457500343937 ], [ 26.450090125054381, 55.132950648077291 ] ] ] } },
-{ "type": "Feature", "properties": { "qc_id": 1, "name": "63-2/3", "descriptio": null, "timestamp": null, "begin": null, "end": null, "altitudemo": null, "tessellate": 1, "extrude": 0, "visibility": -1, "draworder": null, "icon": null }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ 29.450054877096669, 51.406215060096251 ], [ 29.450110175789678, 55.732448327907683 ] ] ] } }
-]
-}
+var json_granici_SK63_C_zone_5 = {"type":"FeatureCollection","name":"granici_SK63_C_zone_5","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[{"type":"Feature","properties":{"fid":"1","qc_id":0.0,"name":"63-1/2","descriptio":null,"timestamp":null,"begin":null,"end":null,"altitudemo":null,"tessellate":"1","extrude":"0","visibility":"-1","draworder":null,"icon":null},"geometry":{"type":"MultiLineString","coordinates":[[[26.450084489033092,51.816457500343937],[26.450090125054381,55.132950648077291]]]}},{"type":"Feature","properties":{"fid":"2","qc_id":1.0,"name":"63-2/3","descriptio":null,"timestamp":null,"begin":null,"end":null,"altitudemo":null,"tessellate":"1","extrude":"0","visibility":"-1","draworder":null,"icon":null},"geometry":{"type":"MultiLineString","coordinates":[[[29.450054877096669,51.406215060096251],[29.450110175789678,55.732448327907683]]]}}]}
